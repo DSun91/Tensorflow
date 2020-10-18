4f323f7c2435e7a7f5afe5357d01b9760baddb64
DSun91 <dialsunny91@gmail.com>
2020-10-18 16:53:55 +0100

    'update'

--- CNN/Transfert Learning/main.py
+++ CNN/Transfert Learning/main.py
@@ -2,10 +2,11 @@ import os
 import zipfile
 import random
 import tensorflow as tf
-import shutil
+from tensorflow.keras.applications import InceptionV3
 from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import shutil
+from tensorflow.keras import Model
 from os import getcwd
 
 path_cats_and_dogs = "cats-and-dogs.zip"
@@ -16,13 +17,13 @@ zip_ref.extractall()
 zip_ref.close()
 
 try:
-    base_dir=os.mkdir('cats-v-dogs/')
-    train=os.mkdir('cats-v-dogs/training/')
-    test=os.mkdir('cats-v-dogs/testing/')
-    train_d=os.mkdir('cats-v-dogs/training/dogs/')
-    train_c=os.mkdir('cats-v-dogs/training/cats/')
-    test_d=os.mkdir('cats-v-dogs/testing/dogs/')
-    test_c=os.mkdir('cats-v-dogs/testing/cats/')
+    base_dir = os.mkdir('cats-v-dogs/')
+    train = os.mkdir('cats-v-dogs/training/')
+    test = os.mkdir('cats-v-dogs/testing/')
+    train_d = os.mkdir('cats-v-dogs/training/dogs/')
+    train_c = os.mkdir('cats-v-dogs/training/cats/')
+    test_d = os.mkdir('cats-v-dogs/testing/dogs/')
+    test_c = os.mkdir('cats-v-dogs/testing/cats/')
 except OSError:
     print(OSError)
     pass
@@ -57,56 +58,65 @@ split_size = .9
 split_data(CAT_SOURCE_DIR, TRAINING_CATS_DIR, TESTING_CATS_DIR, split_size)
 split_data(DOG_SOURCE_DIR, TRAINING_DOGS_DIR, TESTING_DOGS_DIR, split_size)
 
+pre_trained_model = InceptionV3(include_top=False,
+                                weights="imagenet",
+                                input_shape=(150, 150, 3),
+                                )
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(150, 150, 3)),
-    tf.keras.layers.MaxPooling2D(2, 2),
-    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Conv2D(16, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dense(1, activation='sigmoid')
-])
+for layer in pre_trained_model.layers:
+    layer.trainable = False
 
+last_layer = pre_trained_model.get_layer('mixed7')
+print('last layer output shape: ', last_layer.output_shape)
+last_output = last_layer.output
+
+x = tf.keras.layers.Flatten()(last_output)
+
+x = tf.keras.layers.Dense(1024, activation='relu')(x)
+
+x = tf.keras.layers.Dropout(0.2)(x)
+
+x = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+
+model = Model(pre_trained_model.input, x)
+print(model.summary())
 model.compile(optimizer=RMSprop(lr=0.001), loss='binary_crossentropy', metrics=['acc'])
 
-size=150
-train_datagen = ImageDataGenerator(rescale=1/255,
-                                   rotation_range=60,
-                                   width_shift_range=0.3,
-                                   height_shift_range=0.3,
-                                   shear_range=0.2,
-                                   zoom_range=0.2,
+size = 150
+train_datagen = ImageDataGenerator(rescale=1 / 255,
+                                   rotation_range=40,
+                                   width_shift_range=0.1,
+                                   height_shift_range=0.1,
+                                   shear_range=0.1,
+                                   zoom_range=0.1,
                                    horizontal_flip=True,
-                                   fill_mode='nearest')#YOUR CODE HERE
+                                   fill_mode='nearest')  # YOUR CODE HERE
 
 # NOTE: YOU MUST USE A BATCH SIZE OF 10 (batch_size=10) FOR THE
 # TRAIN GENERATOR.
 train_generator = train_datagen.flow_from_directory(
     'cats-v-dogs/training/',
-    target_size=(size,size),
-    batch_size=10,
+    target_size=(size, size),
+    batch_size=20,
     class_mode='binary',
-)#YOUR CODE HERE
+)  # YOUR CODE HERE
 
-validation_datagen = ImageDataGenerator(rescale=1/255,
-                                   rotation_range=60,
-                                   width_shift_range=0.3,
-                                   height_shift_range=0.3,
-                                   shear_range=0.2,
-                                   zoom_range=0.2,
-                                   horizontal_flip=True,
-                                   fill_mode='nearest')#YOUR CODE HERE
+validation_datagen = ImageDataGenerator(rescale=1 / 255,
+                                        rotation_range=40,
+                                        width_shift_range=0.1,
+                                        height_shift_range=0.1,
+                                        shear_range=0.1,
+                                        zoom_range=0.1,
+                                        horizontal_flip=True,
+                                        fill_mode='nearest')  # YOUR CODE HERE
 
 # NOTE: YOU MUST USE A BACTH SIZE OF 10 (batch_size=10) FOR THE
 # VALIDATION GENERATOR.
 validation_generator = validation_datagen.flow_from_directory(
     'cats-v-dogs/testing/',
-    target_size=(size,size),
-    batch_size=10,
-    class_mode='binary')#YOUR CODE HERE
+    target_size=(size, size),
+    batch_size=20,
+    class_mode='binary')  # YOUR CODE HERE
 
 history = model.fit_generator(train_generator,
                               epochs=4,
@@ -115,35 +125,33 @@ history = model.fit_generator(train_generator,
 
 model.save("my_model")
 
-
 import matplotlib.image  as mpimg
 import matplotlib.pyplot as plt
 
-#-----------------------------------------------------------
+# -----------------------------------------------------------
 # Retrieve a list of list results on training and test data
 # sets for each training epoch
-#-----------------------------------------------------------
-acc=history.history['acc']
-val_acc=history.history['val_acc']
-loss=history.history['loss']
-val_loss=history.history['val_loss']
+# -----------------------------------------------------------
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
 
-epochs=range(len(acc)) # Get number of epochs
+epochs = range(len(acc))  # Get number of epochs
 
-#------------------------------------------------
+# ------------------------------------------------
 # Plot training and validation accuracy per epoch
-#------------------------------------------------
+# ------------------------------------------------
 plt.plot(epochs, acc, 'r', "Training Accuracy")
 plt.plot(epochs, val_acc, 'b', "Validation Accuracy")
 plt.title('Training and validation accuracy')
 plt.figure()
 
-#------------------------------------------------
+# ------------------------------------------------
 # Plot training and validation loss per epoch
-#------------------------------------------------
+# ------------------------------------------------
 plt.plot(epochs, loss, 'r', "Training Loss")
 plt.plot(epochs, val_loss, 'b', "Validation Loss")
-
 
 plt.title('Training and validation loss')
 
--- CNN/Transfert Learning/test.py
+++ CNN/Transfert Learning/test.py
@@ -3,21 +3,30 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 import matplotlib.pyplot as plt
 
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-model=tf.keras.models.load_model('my_model')
 
-foto=image.load_img('hh.jpg',target_size=(150,150))
+model=tf.keras.models.load_model('my_model_k_r')
+
+#scores=model.evaluate_generator(generator=validation_generator,steps=1038/20)
+#print(scores)
+foto=image.load_img('kk.png',target_size=(150,150))
 plt.imshow(foto)
 plt.show()
 
 x = image.img_to_array(foto)
 
-x = np.expand_dims(x, axis=0)
+x = np.expand_dims(x/255, axis=0)
 
 images = np.vstack([x])
-classes = model.predict(images, batch_size=10)
+
+
+classes = model.predict(images)
+
 print(classes[0])
 if classes[0]>0.5:
-    print( "Ryu")
+    print( "Dog")
+    print('Ryu')
 else:
-    print("Ken")
+    print("Cat")
+    print('Ken')
